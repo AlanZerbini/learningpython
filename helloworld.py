6648a77b5231a806da16e41dd3f264fdfa5cf4b1
Alan Zerbini <alanfczerbini@gmail.com>
2020-11-02 11:45:21 -0300

    Arquivos adicionados via upload

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+# print('Hello World!')
+print("What's your name?")
+name = input()
+print("Hello, " + name)
---
+++ numbers.py
@@ -0,0 +1,6 @@
+print('First number:')
+first_number = int(input())
+print('Second number:')
+second_number = int(input())
+sum = first_number + second_number
+print("Sum: " + str(sum))
