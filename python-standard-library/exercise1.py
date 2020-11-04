3e7c68a6a033f7c08cf03ea8dd529ef5eb2a0dd1
Alan Zerbini <alanfczerbini@gmail.com>
2020-11-04 19:38:43 -0300

    Add files via upload

---
+++ python-standard-library/exercise1.py
@@ -0,0 +1,3 @@
+import random as dice
+roll = dice.randint(1 , 10)
+print(f'You rolled {roll}.')
---
+++ python-standard-library/exercise2.py
@@ -0,0 +1,4 @@
+py -m pip install emoji
+import emoji
+message = emoji.emojize('Howdy :sun_with_face:')
+print(message)
